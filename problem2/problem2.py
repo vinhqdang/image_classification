2922272c5331a1c405a26a738c5d8b3e8e1cd6c6
Vinh Q. DANG <dqvinh87@gmail.com>
2017-01-27 12:38:56 +0100

    add reports

--- problem2/problem2.py
+++ problem2/problem2.py
@@ -108,7 +108,7 @@ if __name__ == "__main__":
     # this is the augmentation configuration we will use for training
     train_datagen = ImageDataGenerator(
             rescale=args.rescale,
-            rotation_range=90,
+            # rotation_range=90,
             # width_shift_range=0.2,
             # height_shift_range=0.2,
             shear_range=0.2,
